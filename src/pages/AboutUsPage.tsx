3f622bc52477479e9d3f3efe7330fee47d960ad3
Nguyen Duy Hung <hungndse184681@fpt.edu.vn>
2024-10-29 09:15:38 +0700

    Update AboutUsPage.tsx

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -9,7 +9,7 @@ const AboutUsPage: React.FC = () => {
         <div>
             <Navbar />
             <div className="container my-5 d-flex flex-column content-about-us">
-                <h1 className="" style={{ marginTop: '10px' }}>About Us</h1>
+                <h1 className="content-header" >About Us</h1>
                 <p className="content-body" >
                 </p>
                 <div className="container about-section">
@@ -24,39 +24,28 @@ const AboutUsPage: React.FC = () => {
                             <img src={`${vetImg}`} className="vet-img" alt="Veterinarian" />
                         </div>
 
-                        <div className="col-md-8 about-text" style={{ marginTop: '3rem' }}>
+                        <div className="col-md-8 about-text mt-3" >
                             <h2>Our Mission</h2>
                             <p>
-                            Our mission is to provide the highest quality care for your Koi fish, ensuring that they live long, healthy, and happy lives. We are committed to staying up-to-date on the latest advances in Koi fish health and wellness, and we strive to provide compassionate and personalized care for each fish that comes through our doors.
+                                Our mission is to provide the highest quality care for your Koi fish, ensuring that they live long, healthy, and happy lives. We are committed to staying up-to-date on the latest advances in Koi fish health and wellness, and we strive to provide compassionate and personalized care for each fish that comes through our doors.
                             </p>
                         </div>
-                        <div className="col-md-4 about-image" style={{ marginTop: '3rem' }}>
+                        <div className="col-md-4 about-image mt-3" >
                             <img src={`${serviceImg}`} className="vet-img" alt="Veterinarian" />
                         </div>
 
-                        <div className="col-md-8 about-text" style={{ marginTop: '3rem' }}>
+                        <div className="col-md-8 about-text mt-3" >
                             <h2>Our Commitment</h2>
                             <p>
-                            Our specialists work together to provide a full range of services, from routine health check-ups to emergency care. We prioritize the well-being of your Koi and aim to educate owners about best practices in Koi husbandry.
+                                Our specialists work together to provide a full range of services, from routine health check-ups to emergency care. We prioritize the well-being of your Koi and aim to educate owners about best practices in Koi husbandry.
 
-Whether you’re a seasoned Koi keeper or a newcomer to the hobby, our team is here to support you every step of the way. Trust us to provide the expert care your Koi deserve!
+                                Whether you’re a seasoned Koi keeper or a newcomer to the hobby, our team is here to support you every step of the way. Trust us to provide the expert care your Koi deserve!
                             </p>
                         </div>
-                        <div className="col-md-4 about-image" style={{ marginTop: '3rem' , marginBottom: '3rem'}}>
+                        <div className="col-md-4 about-image" style={{ marginTop: '3rem', marginBottom: '3rem' }}>
                             <img src={`${homepage2}`} className="vet-img" alt="Veterinarian" />
                         </div>
 
-                        {/* <div className="col-md-12 about-text">
-                        <div className="col-md-8 about-text" style={{ marginTop: '3rem' }}>
-                            <h2>Our Mission</h2>
-                            <p>
-                            Our mission is to provide the highest quality care for your Koi fish, ensuring that they live long, healthy, and happy lives. We are committed to staying up-to-date on the latest advances in Koi fish health and wellness, and we strive to provide compassionate and personalized care for each fish that comes through our doors.
-                            </p>
-                        </div>
-                        <div className="col-md-4 about-image" style={{ marginTop: '3rem' }}>
-                            <img src={`${serviceImg}`} className="vet-img" alt="Veterinarian" />
-                        </div>
-                        </div> */}
                     </div>
                 </div>
             </div>
